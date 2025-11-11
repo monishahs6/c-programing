a03f109325145d563bbc6bc8767c1dfe551513ff
monishahs6 <monishahs784@gmail.com>
2025-11-11 16:19:38 +0530

    Add files via upload

---
+++ student grade based on total marks - Copy.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    float m1,m2,m3,total,average;
+    char grade;
+    printf("Enter marks for three subjects (outof 100):");
+    scanf("%f %f %f",&m1,&m2,&m3);
+    total=m1+m2+m3;
+    average=total/3;
+    printf("\ntotal marks=%.2f",average);
+    if(average>=90)
+        grade='A';
+        else if(average>=80)
+        grade='B';
+        else if(average>=70)
+        grade='C';
+        else if (average>=60)
+        grade='D';
+        else if(average>=50)
+        grade='E';
+    else
+        grade='F';
+    printf("\ngrade=%c",grade);
+    if(average>=50)
+        printf("\nresult:PASS\n");
+    else
+        printf("\nResult:FAIL\n");
+    return 0;
+}
